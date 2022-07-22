9fbbc330d6754ced69b10f80b459c07df3f02281
DESKTOP-3PFTMF2\admin <krishna.bhati@orchids.edu.in>
2022-07-22 18:29:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Grid,Card, Typography } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const api = () => {
+    axios.get(`https://api.tvmaze.com/shows`).then((r) => {
+      setData(r?.data);
+      console.log(r, "kkk");
+    });
+  };
+
+  useEffect(() => api(), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container xs={12} item spacing={2} style={{padding : 20}}>
+      {data.map((i) => {
+        return (
+          <Grid xs={12} sm={6} md={3} item onClick={()=>window.open(i?.url)} style={{cursor:'pointer'}}>
+            <Card variant="outlined">
+              {" "}
+              <img src={i?.image?.medium} width="100%" height='100%'/>
+              <Typography style={{
+                // position: 'absolute',
+                // top: 420,
+                color: 'white',
+                marginTop: -40,
+                marginLeft : 10,
+                fontSize: 22,
+                fontWeight: 700,
+              }}>{i?.name}</Typography>
+            </Card>
+          </Grid>
+        );
+      })}
+    </Grid>
   );
 }
 
